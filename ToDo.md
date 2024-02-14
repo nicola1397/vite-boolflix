dbc0534d80c181063b8ecdd4c5dee04085088beb
nicola1397 <nicola1397@outlook.it>
2024-02-14 22:24:48 +0100

    feat: added result filter, progressive search

---
+++ ToDo.md
@@ -0,0 +1,3 @@
+# PROBLEMI DA RISOLVERE
+
+- L'API da 20 risultati per volta -> Scorrere le pagine e refreshare l'array o iterare tutte le pagine e aggiungerle
--- src/App.vue
+++ src/App.vue
@@ -15,28 +15,56 @@ export default {
       axios
         .get(`${store.searchStr}${this.title}${store.apiKey}`)
 
-        .then((response) => (store.movies = response.data.results));
+        .then((response) => {
+          const allResults = response.data.results;
+          store.searchObj = allResults.filter(
+            (result) => result.media_type != "person"
+          );
+        });
     },
   },
 };
 </script>
 
 <template>
-  <input
-    class="form-control"
-    type="search"
-    placeholder="Search"
-    v-model="title"
-  />
-  <button class="btn btn-danger" @click="fetch()">Search</button>
+  <div class="input-group">
+    <input
+      class="form-control"
+      type="search"
+      placeholder="Search"
+      v-model="title"
+      @keydown="fetch()"
+    />
+    <button class="btn btn-danger" @click="fetch()">Search</button>
+  </div>
 
   <ul>
-    <li v-for="movie in store.movies">
-      <h1>{{ movie.title }}</h1>
-      <h3>{{ movie.original_title }}</h3>
-      <span>{{ movie.original_language }}</span>
+    <li v-for="media in store.searchObj">
+      <div class="card">
+        <div class="card-header">
+          <h2>{{ media.title || media.name }}</h2>
+        </div>
+        <div class="card-body">
+          <h3>{{ media.original_title || media.original_name }}</h3>
+          <h5>{{ media.original_language }}</h5>
+          <span>{{ media.vote_average }}</span>
+        </div>
+      </div>
     </li>
   </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px;
+}
+
+.input-group {
+  margin: 10px;
+}
+</style>
